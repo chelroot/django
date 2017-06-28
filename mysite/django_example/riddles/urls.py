a2b631304c24608c7abb9bc6854bb7b66b5c7bef
Your Name <you@example.com>
2017-06-28 14:48:22 +0500

    Initial commit

---
+++ mysite/django_example/riddles/urls.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# coding: utf-8
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'riddles'
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
